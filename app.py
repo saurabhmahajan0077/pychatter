0ce6a4e66364af63ec1218e9b1526777ef4d1b32
Saurabh Mahajan <saurabhmahajan0077@gmail.com>
2017-04-14 15:42:09 +0530

    --added at from environment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 # This needs to be filled with the Page Access Token that will be provided
 # by the Facebook App that will be created.
-PAT = ''
+PAT = os.environ.get('at')
 
 @app.route('/', methods=['GET'])
 def handle_verification():
